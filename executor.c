02578079b08f162d10e16c177e3610e256d0bd8e
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-08-23 16:11:03 +0100

    Update

--- executor.c
+++ executor.c
@@ -7,29 +7,40 @@
  * Return: If an error occurs - a corresponding error code.
  *         if sucessful - The exit value of the last executed command.
  */
-void execute(char **args) {
+void execute(char **args)
+{
     pid_t pid;
     
     pid = fork();
-
-    if (pid == -1) {
+    
+    if (pid == -1)
+    {
         perror("fork");
-    } else if (pid == 0) {
+    }
+    else if (pid == 0)
+    {
         int exec_result = execve(args[0], args, NULL);
 
-        if (exec_result == -1) {
+        if (exec_result == -1)
+        {
             perror("execve");
             exit(EXIT_FAILURE);
         }
-    } else {
+    }
+    else
+    {
         int status;
         waitpid(pid, &status, 0);
 
-        if (WIFEXITED(status)) {
-            if (WEXITSTATUS(status) != 0) {
+        if (WIFEXITED(status))
+        {
+            if (WEXITSTATUS(status) != 0)
+            {
                 printf("Command failed with exit status %d\n", WEXITSTATUS(status));
             }
-        } else if (WIFSIGNALED(status)) {
+        }
+        else if (WIFSIGNALED(status))
+        {
             printf("Command terminated by signal %d\n", WTERMSIG(status));
         }
     }
